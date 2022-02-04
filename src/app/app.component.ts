8f3ca43eef8accd8c0e630a92305a48a7bfadf8e
Potyv <poty.vinent@icloud.com>
2022-02-04 09:49:57 +0100

    object snapFace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap} from '../models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  mySnap!: FaceSnap;
+
+  ngOnInit(){
+    this.mySnap = new FaceSnap(
+       'Fleurs',
+        'Belle vue d\'un jardin',
+        new Date(),
+        6,
+        'https://www.deco.fr/sites/default/files/styles/article_970x500/public/2019-06/shutterstock_190731452.webp?itok=D6Yv9kkF',
+    );
+  }
+}
